2a115ff6994b4dc870d8d7fc4f2a3125c9fc1bbf
obabs <43038248+obabs@users.noreply.github.com>
2018-09-10 18:19:37 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,9 +3,4 @@ node default {
  node 'puppetsvr.domain' {
  include role::master_server
 }
-  file {'/etc/issue':
-    ensure   => file,
-    content  => 'This is the Puppet Client',
-    owner    => 'root',
-    }
-  }
+  
